25dc1468c4722a800fa9e6c9950314e5dbe32d25
elinalak <lakida_v@icloud.com>
2022-10-22 16:31:06 +0200

    Optimized search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,6 +6,7 @@ import Result from "./Result";
 export default function Dictionary() {
   let [word, setWord] = useState("");
   let [response, setResponse] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function showResponse(response) {
     setResponse(response.data[0]);
@@ -15,6 +16,10 @@ export default function Dictionary() {
   function handleSubmit(event) {
     event.preventDefault();
     console.log("Is submitting");
+    search();
+  }
+
+  function search() {
     console.log(word);
     //documentation: https://dictionaryapi.dev/
     let APIurl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
@@ -25,19 +30,27 @@ export default function Dictionary() {
     setWord(event.target.value);
   }
 
-  return (
-    <div>
-      <form className="mt-4" onSubmit={handleSubmit}>
-        <header className="Header-name mt-3">Dictionary</header>
-        <input
-          className="mt-2"
-          type="search"
-          placeholder="Type any word.."
-          autoFocus="on"
-          onChange={changeWord}
-        ></input>
-      </form>
-      <Result response={response} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded)
+    return (
+      <div>
+        <form className="mt-4" onSubmit={handleSubmit}>
+          <header className="Header-name mt-3">Dictionary</header>
+          <input
+            className="mt-2"
+            type="search"
+            placeholder="Type any word.."
+            autoFocus="on"
+            onChange={changeWord}
+          ></input>
+        </form>
+        <Result response={response} />
+      </div>
+    );
+  else load();
+  return "Loading";
 }
